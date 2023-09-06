5a9917820e644d907d59934852d114828e862966
Clarke2302 <cladreamer@outlook.com>
2023-09-06 17:21:40 -0300

    add  hello world

---
+++ hello.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class hello{
+public static void main(String [] args){
+Scanner tec = new Scanner(System.in);
+
+System.out.println("---------------------------------------------");
+System.out.print("Digite o seu nome e se quiser seu sobrenome: ");
+String nome = tec.nextLine();
+
+System.out.println("\n"+"Hello World for you, "+nome+"! :)"+"\n---------------------------------------------");
+
+
+
+
+
+    }
+}
