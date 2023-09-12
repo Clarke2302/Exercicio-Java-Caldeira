3f5ad146a1282cf415e7cb991cf558489bb04f0f
Clarke Brasil <cladreamer@outlook.com>
2023-09-12 16:30:23 -0300

    Create exercico2.java
    adicionando exercicio 2

---
+++ aula1/exercico2.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class exercico2 {
+    public static void main(String[]args) {
+        int valorUm = 8;
+        double valorDois = 3.5;
+
+        System.out.println(valorUm+"\n"+valorDois);
+
+    }
+}
