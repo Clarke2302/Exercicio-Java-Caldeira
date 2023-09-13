cd1d5b60d290cc271b0156fbd81ba92c8157b844
Clarke2302 <cladreamer@outlook.com>
2023-09-13 20:27:35 -0300

    add exercicio 5

---
+++ aula1/exercicio5.java
@@ -0,0 +1,16 @@
+public class exercicio5 {
+    public static void main(String[]args){ //dificuldades
+
+        int hora = 2;
+        int minuto = 23;
+        int segundo = 3;
+
+        int horaPMin= hora*60;
+        int minPSeg = (horaPMin+minuto)/60;
+        int contaSegundos = minPSeg+segundo;
+        int contaMeiaNoite = ;
+
+        System.out.println(contaSegundos+" "+contaMeiaNoite);
+    }
+    
+}
