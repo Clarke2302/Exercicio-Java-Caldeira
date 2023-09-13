a1ea4ebf26dd4b12c31ad0a9d76c7fba84907aa7
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-09-13 20:37:58 -0300

    Update and rename exercico2.java to exercicio2.java

--- aula1/exercicio2.java
+++ aula1/exercicio2.java
@@ -1,6 +1,6 @@
 import org.w3c.dom.ls.LSOutput;
 
-public class exercico2 {
+public class exercicio2 {
     public static void main(String[]args) {
         int valorUm = 8;
         double valorDois = 3.5;
