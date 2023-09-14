556b585fbc9e585988fa85c68d594c932c8306fa
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-09-14 14:33:48 -0300

    Update exercicio5.java

--- aula1/exercicio5.java
+++ aula1/exercicio5.java
@@ -1,16 +1,16 @@
 public class exercicio5 {
     public static void main(String[]args){ //dificuldades
-
+//quantidade de segundos que se passaram desde as 0h00min0s e a quantidade de segundos que faltam para a meia-noite
         int hora = 2;
         int minuto = 23;
         int segundo = 3;
 
-        int horaPMin= hora*60;
-        int minPSeg = (horaPMin+minuto)/60;
-        int contaSegundos = minPSeg+segundo;
-        int contaMeiaNoite = ;
+        int horaPMin= hora*3600; //hora dividido por 1 hora = 3660 seg
+        int minPSeg = horaPMin+(minuto*60); //hora em seg + minuto para seg 1 min = 60 seg
+        int contaSegundos = minPSeg+segundo; //soma tudo e vira segundo
+        int contaMeiaNoite = contaSegundos ;
 
-        System.out.println(contaSegundos+" "+contaMeiaNoite);
+        System.out.println("Quantos segundos se passaram desde às 0h00min0s? "+contaSegundos+" segundos"+"/n"+"Quantos segundos faltam para a meia-noite? "+contaMeiaNoite+" segundos");
     }
     
 }
