d50bc0b316cbfb7a9b4f8e00a61c501c5fbd303f
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-09-19 16:19:48 -0300

    adicionando
    add exercicio 5

---
+++ aula2/exercicio5.java
@@ -0,0 +1,17 @@
+public class exercicio5 {
+    //Escreva um programa que separa a string em duas metades
+    // e imprime a primeira parte.
+
+    public static void main (String[]args){
+
+        String frase = "Helena ama coelhos, ela tem um de pelúcia.";
+        System.out.println("Frase sem edição: "+"\n"+"\n"+frase+"\n");
+
+        String [] editada = frase.split(",");
+
+        System.out.println("Frase com edição:"+"\n"+"\n"+editada[0]+"\n"+editada[1]);
+
+
+
+    }
+}
