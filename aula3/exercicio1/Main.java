c36b324c9afd3b6a73a8b88eadd33a318f21817d
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-09-21 16:45:37 -0300

    Create Main.java
    adicionei classe Main para rodar as contas

---
+++ aula3/exercicio1/Main.java
@@ -0,0 +1,31 @@
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        ContaBancaria cb = new ContaBancaria("Ronaldo Guto", "367.748.738-36",53627272,"CBS Bank","Rua dos Monges, 123",1500);
+
+
+        System.out.println("\n");
+
+        //saque
+        cb.verificarSaldo();
+        cb.saque(200);
+        cb.verificarSaldo();
+
+        //deposito
+        cb.deposito(500);
+        cb.verificarSaldo();
+
+        //ver hora
+        cb.verificarHorario();
+        //ver info contaBancaria cb
+        cb.verificarInfo(cb);
+
+        //faz pix
+        cb.pix(4000);
+
+        
+
+
+    }
+}
