3e09a654159178c28dbe1c5e171b9039ad7088b7
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-09-23 00:04:12 -0300

    Create FormaGeometrica.java
    adicionei rascunho do exercicio 2 da aula 4

---
+++ aula4/exercicio2/FormaGeometrica.java
@@ -0,0 +1,28 @@
+package exercicio2;
+
+public abstract class FormaGeometrica { //classe abstrata
+    
+    public abstract calcularArea(){
+
+    }
+}
+
+public void Retangulo(){
+calcularArea();
+}
+
+public void Circulo(){
+calcularArea();
+}
+
+public void Triangulo(){
+calcularArea();
+}
+
+public void Quadrado(){
+calcularArea();
+}
+
+public void Losango(){
+calcularArea();
+}
