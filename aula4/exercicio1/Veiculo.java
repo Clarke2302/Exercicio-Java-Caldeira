363c2f72f2527722f4d3f870f0f469b20758521e
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-09-24 23:44:32 -0300

    Update Veiculo.java
    atualizada a classe abstrata VEICULO

--- aula4/exercicio1/Veiculo.java
+++ aula4/exercicio1/Veiculo.java
@@ -1,23 +1,7 @@
 package exercicio1;
-
-public abstract class Veiculo { //classe abstrata
+public abstract class Veiculo { //classe abstrata não pode criar objetos nela, somente em suas subclasses
     String marca;
     String modelo;
     int ano;
-}
-
-public abstract calcularCustoViagem(int distancia){ //método abstrato
-    //calcular o custo de uma viagem com bas e na distância fornecida.
-
-}
-
-//duas classes concretas que herdem de Veiculo
-
-public void Carro(){
-    double custoPorKm = 0.20;
-calcularCustoViagem();
-}
-public void Moto(){
-    double custoPorKm = 0.15;
-calcularCustoViagem();
+    public abstract double calcularCustoViagem();
 }
