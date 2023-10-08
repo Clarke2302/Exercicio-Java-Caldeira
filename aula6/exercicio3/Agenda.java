39462e05cb07b9d8910cebe2841079899ed1c0db
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-08 17:54:38 -0300

    Create Agenda.java

---
+++ aula6/exercicio3/Agenda.java
@@ -0,0 +1,10 @@
+package exercicio3;
+
+public class Agenda {
+    
+    //Crie um programa Java que utilize uma estrutura de dados do tipo TreeMap para criar uma agenda telefônica.
+    //Agenda deve conter: número de telefone, nome, endereço e local da cidade (norte, sul, centro, leste e oeste).
+    //Associe nomes a números de telefone e, em seguida,
+    // imprima a lista de contatos em ordem alfabética e imprima os contatos com base na localidade.
+    
+}
