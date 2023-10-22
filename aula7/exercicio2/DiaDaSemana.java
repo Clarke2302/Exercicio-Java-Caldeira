43a42d54378f3cfba53383843a25eb1b418345b3
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-22 20:36:50 -0300

    Create DiaDaSemana.java

---
+++ aula7/exercicio2/DiaDaSemana.java
@@ -0,0 +1,8 @@
+package exercicio2;
+
+public enum DiaDaSemana {
+    //Crie um enum chamado DiaDaSemana que represente os dias da semana
+    //(por exemplo, SEGUNDA, TERCA, etc.).
+    
+    SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO;
+}
