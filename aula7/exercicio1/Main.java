020146ce2e0c66508eb68bc792f0462c7f088c1c
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-22 19:55:16 -0300

    Create Main.java
    Main criada para testar funções iguais da interface mas com prints diferentes.

---
+++ aula7/exercicio1/Main.java
@@ -0,0 +1,16 @@
+package exercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Circulo c1 = new Circulo();
+        Quadrado q1 = new Quadrado();
+
+        System.out.println("================================="+"\n"+"Função no circulo: ");
+        c1.desenhar();
+        System.out.println("================================="+"\n"+"Função no Quadrado: ");
+        q1.desenhar();
+
+
+    }
+}
