949bb20b667e34fa3d26fba77030ebc1dfbfe95c
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-22 16:22:32 -0300

    Update Agenda.java
    Foi adicionada descrição e exemplo de código para testar. Também foi adicionado um TreeMap para armazenar pessoas na agenda, um menu interativo para usuário e funções.

--- aula6/exercicio3/Agenda.java
+++ aula6/exercicio3/Agenda.java
@@ -1,19 +1,17 @@
 package exercicio3;
-import java.util.TreeMap;
 
 public class Agenda {
-    public static void main(String[] args) {
-        
-    
     //Crie um programa Java que utilize uma estrutura de dados do tipo TreeMap para criar uma agenda telefônica.
     //Agenda deve conter: número de telefone, nome, endereço e local da cidade (norte, sul, centro, leste e oeste).
-    //Associe nomes a números de telefone e, em seguida,
-    // imprima a lista de contatos em ordem alfabética e imprima os contatos com base na localidade.
-    
-    TreeMap<Integer, String, String, String> agenda = new TreeMap<Integer, String, String, String>(3);
+        
+    String nomeCompleto;
+    String endereco;
+    String localCidade;
 
-    
+    public Agenda(String nomeCompleto,String endereco,String localCidade){
+        this.nomeCompleto = nomeCompleto;
+        this.endereco = endereco;
+        this.localCidade=localCidade;
     }
 
-    //funções
 }
