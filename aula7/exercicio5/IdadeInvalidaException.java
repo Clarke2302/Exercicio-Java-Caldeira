e7e479984b8fc23c2073a99ca67bcb9c3b2418b7
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-22 20:47:54 -0300

    Criando exercicio5

---
+++ aula7/exercicio5/IdadeInvalidaException.java
@@ -0,0 +1,5 @@
+package exercicio5;
+
+public class IdadeInvalidaException extends RuntimeException{
+    //Crie uma exceção personalizada chamada IdadeInvalidaException que herde de RuntimeException.
+}
---
+++ aula7/exercicio5/Main.java
@@ -0,0 +1,12 @@
+package exercicio5;
+
+public class Main {
+    public static void main(String[] args) {
+//Em um programa, peça ao usuário que insira sua idade.
+//Se a idade for menor que 0 ou maior que 150, lance a exceção IdadeInvalidaException
+// com uma mensagem apropriada.
+
+
+
+    }
+}
