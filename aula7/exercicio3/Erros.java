1886dd4dc707e84de0c30ae9e316cb5b38e92b0f
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-22 20:47:13 -0300

    Create Erros.java

---
+++ aula7/exercicio3/Erros.java
@@ -0,0 +1,12 @@
+package exercicio3;
+
+public class Erros {
+
+    //Crie um programa que solicite ao usuário que insira dois números inteiros.
+    //Utilize tratamento de exceções para lidar com a possibilidade de entrada inválida
+    //(por exemplo, uma entrada que não seja um número inteiro)
+    //exiba uma mensagem de erro apropriada.
+
+    
+    
+}
