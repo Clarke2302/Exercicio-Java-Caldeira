777e3c3d43c8227904b82ae5f5de2344c6d94a77
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-22 18:52:59 -0300

    Update testaAgenda.java

--- aula6/exercicio3/testaAgenda.java
+++ aula6/exercicio3/testaAgenda.java
@@ -18,9 +18,14 @@ public class testaAgenda {
         System.out.println("Value of A: " + valueA);
     */
 
-    TreeMap<Agenda, Integer> agenda = new TreeMap<Agenda, Integer>();
+    TreeMap<String, Integer> agenda = new TreeMap<String, Integer>();
     Scanner tec = new Scanner (System.in);
 
+        //Agenda agi1 = new Agenda("Claudia Brasil", "Rua dos ladrões, 171", "Sul");
+        //Agenda agi2 = new Agenda("Ana Rosa","Beco das Pétalas, 325", "norte");
+
+        //agenda.put("Claudia Brasil", 97382647 );
+        //agenda.put("Ana Rosa", 97462384);
 
         int tecla = 1;
         while(tecla!=0){
@@ -51,33 +56,38 @@ public class testaAgenda {
 
     //funções
 
-    public static void cadastraNaAgenda(TreeMap<Agenda, Integer> nome, Scanner tec){
+    public static void cadastraNaAgenda(TreeMap<String, Integer> nome, Scanner tec){
         tec.nextLine();
-        System.out.println("\n"+"Cadastrando pessoa na Agenda:"+"\n"+
-        "Digite o nome completo da pessoa: "+"\n");
+        System.out.println("\n"+"Cadastrando pessoa na Agenda:"+"\n"+"============================================"+"\n"+
+        "Digite o nome completo da pessoa: ");
         String nomeComp = tec.nextLine();
-        System.out.println("Digite o número de telefone sem DDD: "+"\n");
+        System.out.println("\n"+"Digite o número de telefone sem DDD: ");
         int numero = tec.nextInt();
-        System.out.println("Digite o endereço com nº dessa pessoa: "+"\n");
+        tec.nextLine(); //buffer
+        System.out.println("\n"+"Digite o endereço com nº dessa pessoa: ");
         String endere = tec.nextLine();
-        System.out.println("Digite a zona da cidade que essa pessoa mora (norte, sul, centro, leste e oeste): ");
+        System.out.println("\n"+"Digite a zona da cidade que essa pessoa mora (norte, sul, centro, leste e oeste): ");
         String zona = tec.nextLine();
         
-        Agenda ag = new Agenda(nomeComp,endere,zona);
-        nome.put(ag,numero);
+        //Agenda ag = new Agenda(nomeComp,endere,zona);
+        String dadosPessoais=(nomeComp+"\n"+endere+"\n"+zona);
+        nome.put(dadosPessoais,numero);
+        
         //Associe nomes a números de telefone e, em seguida, ordenar lista em ordem alfabética
+        System.out.println("\n"+"Cadastro efetuado!"+"\n");
     
     }
     
-    public static void mostraNaAgenda(TreeMap<Agenda, Integer> nome, Scanner tec){// imprima a lista de contatos em ordem alfabética
-        for(int i=0; i<nome.size(); i++){
+    public static void mostraNaAgenda(TreeMap<String, Integer> nome, Scanner tec){// imprima a lista de contatos em ordem alfabética
+        System.out.println("\n"+"Mostrando Agenda em ordem alfabética: "+"\n"+"============================================"+"\n");
             
-            System.out.println(nome);
-        }
+            System.out.println(nome+"\n");
+        
         
     }
-    public static void mostraNaAgendaZona(TreeMap<Agenda, Integer> nome, Scanner tec){//imprima os contatos com base na localidade.
-        System.out.println("Mostrando pessoas pela zona: ");
+    public static void mostraNaAgendaZona(TreeMap<String, Integer> nome, Scanner tec){//imprima os contatos com base na localidade.
+        tec.nextLine();
+        System.out.println("\n"+"Mostrando pessoas da Agenda pela zona: "+"\n"+"============================================"+"\n");
         System.out.println("Digite a zona das pessoas que deseja procurar: ");
         String zonaDigitada = tec.nextLine().toLowerCase();
         
@@ -86,6 +96,8 @@ public class testaAgenda {
 
                 System.out.println(nome);
             }
+        }else{
+            System.out.println("\n"+"Não encontramos nenhuma pessoa nessa zona da cidade!"+"\n");
         }
     }
 }
