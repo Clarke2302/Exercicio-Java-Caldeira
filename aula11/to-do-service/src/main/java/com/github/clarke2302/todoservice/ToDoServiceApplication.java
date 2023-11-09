2bfbdb38c5a745973c8a6d7663dc3deec2fefc42
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-11-09 16:45:13 -0300

    adicionando api to do service

---
+++ aula11/to-do-service/src/main/java/com/github/clarke2302/todoservice/ToDoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.clarke2302.todoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoServiceApplication.class, args);
+	}
+
+}
