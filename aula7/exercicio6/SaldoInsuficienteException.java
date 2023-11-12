a87ae5c4738f3c0c3a88eaabce4876ae3c7e835b
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-11-12 20:07:31 -0300

    Exceção Criada

--- aula7/exercicio6/SaldoInsuficienteException.java
+++ aula7/exercicio6/SaldoInsuficienteException.java
@@ -1,5 +1,9 @@
 package exercicio6;
 
-public class SaldoInsuficienteException extends Exception{
+public class SaldoInsuficienteException extends Exception {
     //Crie uma exceção personalizada chamada SaldoInsuficienteException que herde de Exception.
+
+    public SaldoInsuficienteException(String msg){
+        super(msg);
+    }
 }
