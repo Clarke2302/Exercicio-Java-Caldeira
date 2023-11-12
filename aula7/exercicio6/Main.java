05e8de51ce64578b17650a092b0034cb502240b3
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-11-12 20:09:30 -0300

    Update Main.java
    Colocando teste de função sacar e mostrando a exceção de erro criada

--- aula7/exercicio6/Main.java
+++ aula7/exercicio6/Main.java
@@ -1,9 +1,16 @@
 package exercicio6;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SaldoInsuficienteException {
         
         //Em um programa principal, trate essa exceção e forneça uma mensagem de erro adequada.
+
+        ContaBancaria c1 = new ContaBancaria(2000);
+        ContaBancaria c2 = new ContaBancaria(200);
+
+        c1.sacar(700); //Dá certo
+        
+        c2.sacar(300); //Dá Erro
 
     }
 }
