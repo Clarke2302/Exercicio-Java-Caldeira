e84be7119164fac08698844bfb270bbd3a81c2d5
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-11-12 20:08:52 -0300

    Criada classe ContaBancaria
    Adicionando método construtor, função sacar e lidando com erros de saldo insuficiente.

--- aula7/exercicio6/ContaBancaria.java
+++ aula7/exercicio6/ContaBancaria.java
@@ -4,4 +4,20 @@ public class ContaBancaria {
  //Crie uma classe ContaBancaria que represente uma conta com um saldo inicial.
  // Escreva um método sacar(double valor) na classe ContaBancaria que lance a exceção
  // SaldoInsuficienteException se o saldo for insuficiente para a retirada.
+
+    double saldoInicial;
+
+    public ContaBancaria(double saldoInicial){ //construtor
+        this.saldoInicial=saldoInicial;
+    }
+
+    public void sacar (double valor) throws SaldoInsuficienteException {
+        if(valor>saldoInicial){
+            throw new SaldoInsuficienteException ("Seu saldo é insuficiente");
+        }else{
+            saldoInicial -= valor;
+            System.out.println("\n"+"Você sacou R$"+valor+"."+"\n"+"Agora seu saldo é de R$ "+saldoInicial);
+        }
+    }
+
 }
